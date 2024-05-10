c27f065a7cc3aa2590fb279be47dc14ade799697
helene <helene.maja@hotmail.dk>
2024-05-10 22:13:43 +0200

    main.c file to print a string

--- to_do_list/main.c
+++ to_do_list/main.c
@@ -12,14 +12,28 @@ int main() {
     }
 
     printf("e-Paper Init and Clear...\r\n");
-    EPD_4IN2_V2_Init();
-    EPD_4IN2_V2_Clear();
-    
-    // Initialize for 4-gray level display
-    // EPD_4IN2_V2_Init_4Gray();
-    printf("Drawing string..\n");
-    Paint_DrawString_EN(10, 0, "waveshare", &Font16, BLACK, WHITE);
+    EPD_4IN2_V2_Init();  // Initialize the e-Paper display
+    EPD_4IN2_V2_Clear();  // Clear the display
+    DEV_Delay_ms(500);  // Delay to allow the display to refresh
 
+    // Create a new image cache
+    UBYTE *BlackImage;
+    UWORD Imagesize = ((EPD_4IN2_V2_WIDTH % 8 == 0)? (EPD_4IN2_V2_WIDTH / 8): (EPD_4IN2_V2_WIDTH / 8 + 1)) * EPD_4IN2_V2_HEIGHT;
+    if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+        printf("Failed to apply for black memory...\r\n");
+        return -1;  // Check for successful memory allocation
+    }
+    Paint_NewImage(BlackImage, EPD_4IN2_V2_WIDTH, EPD_4IN2_V2_HEIGHT, 0, WHITE);  // Create a new image
+
+    // Select the image for drawing
+    Paint_SelectImage(BlackImage);
+    Paint_Clear(WHITE);  // Clear the image with a white background
+    printf("drawing string...\r\n");
+    // Draw a string in English
+    Paint_DrawString_EN(10, 20, "hello world", &Font12, BLACK, WHITE);  // Position at x=10, y=20, font size 12
+    printf("drew string...\r\n");
+    // Display the image on the e-Paper
+    EPD_4IN2_V2_Display(BlackImage);
     // Wait for a few seconds to observe the image
     DEV_Delay_ms(2000);
 
