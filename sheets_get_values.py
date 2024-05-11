d5b90297ff7574964e765420d817a38150e617db
helene <helene.maja@hotmail.dk>
2024-05-11 19:06:50 +0200

    add function to write data from sheet to txt file

--- sheets_get_values.py
+++ sheets_get_values.py
@@ -12,5 +12,13 @@ def get_values(spreadsheet_id, creds):
         .execute()
     )
     data = result.get("values", [])
+    # write the data from sheet to a txt doc
+    output_for_data = "".join(sublist[0] for sublist in data)
+    write_to_doc("text.txt", output_for_data)
     output = ", ".join(sublist[0] for sublist in data)
     print(output)
+
+def write_to_doc(file, data):
+    with open(file, 'w') as file:
+        for value in data:
+            file.write(f"{value}\n")
